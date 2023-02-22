8e084cdbe88dfb4a703dd44dbc7ec60e105e9dde
Abbas mkhzomi <llabbasmkhll@gmail.com>
2023-02-22 04:49:33 +0330

    create sortable trait

---
+++ src/Traits/Sortable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Abbasudo\LaravelPurity\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+
+trait Sortable
+{
+    /**
+     * Apply sorts to the query builder instance
+     *
+     * @param  Builder  $query
+     *
+     * @return Builder
+     */
+    public function scopeSort(Builder $query): Builder
+    {
+        $fields = request('sort', []);
+
+        if ( ! is_array($fields)) {
+            $fields = [$fields];
+        }
+
+        foreach ($fields as $field) {
+            $column = Str::of($field)->beforeLast(':');
+
+            if (Str::of($field)->endsWith(':desc')) {
+                $query->orderByDesc($column);
+            } else {
+                $query->orderBy($column);
+            }
+        }
+
+        return $query;
+    }
+
+}
