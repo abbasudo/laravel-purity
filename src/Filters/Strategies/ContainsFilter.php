dbb59f7f45ea2face0012de47fd38e848a19e328
f.mohammadi <f.mohammadi@behaminplus.ir>
2023-02-25 15:26:30 +0330

    add contains filter

---
+++ src/Filters/Strategies/ContainsFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Abbasudo\Purity\Filters\Strategies;
+
+use Abbasudo\Purity\Filters\Filter;
+use Closure;
+
+class ContainsFilter extends Filter
+{
+    /**
+     * Operator string to detect in the query params.
+     *
+     * @var string
+     */
+    protected static string $operator = '$contains';
+
+    /**
+     * Apply filter logic to $query.
+     *
+     * @return Closure
+     */
+    public function apply(): Closure
+    {
+        return function ($query) {
+            foreach ($this->filters as $filter) {
+                $query->where($this->column, 'like', "%{$filter}%");
+            }
+        };
+    }
+}
