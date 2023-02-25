bc2893c499de734bfa37f286e1be00865190cc51
f.mohammadi <f.mohammadi@behaminplus.ir>
2023-02-25 15:27:38 +0330

    add notContains filter

---
+++ src/Filters/Strategies/NotContainsFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Abbasudo\Purity\Filters\Strategies;
+
+use Abbasudo\Purity\Filters\Filter;
+use Closure;
+
+class NotContainsFilter extends Filter
+{
+    /**
+     * Operator string to detect in the query params.
+     *
+     * @var string
+     */
+    protected static string $operator = '$notContains';
+
+    /**
+     * Apply filter logic to $query.
+     *
+     * @return Closure
+     */
+    public function apply(): Closure
+    {
+        return function ($query) {
+            foreach ($this->filters as $filter) {
+                $query->where($this->column, 'not like', "%{$filter}%");
+            }
+        };
+    }
+}
