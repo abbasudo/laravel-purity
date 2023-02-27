2a6e7a9554195a83d59dd29ad680ce082ac0a5e8
phrshteh <frmohammadi007@gmail.com>
2023-02-27 17:45:32 +0330

    add filters list

--- config/purity.php
+++ config/purity.php
@@ -1,12 +1,38 @@
 <?php
 
+use Abbasudo\Purity\Filters\Strategies\BetweenFilter;
+use Abbasudo\Purity\Filters\Strategies\ContainsFilter;
+use Abbasudo\Purity\Filters\Strategies\EndsWithFilter;
 use Abbasudo\Purity\Filters\Strategies\EqualFilter;
+use Abbasudo\Purity\Filters\Strategies\GreaterOrEqualFilter;
+use Abbasudo\Purity\Filters\Strategies\GreaterThanFilter;
 use Abbasudo\Purity\Filters\Strategies\InFilter;
+use Abbasudo\Purity\Filters\Strategies\LessOrEqualFilter;
+use Abbasudo\Purity\Filters\Strategies\LessThanFilter;
+use Abbasudo\Purity\Filters\Strategies\NotContainsFilter;
+use Abbasudo\Purity\Filters\Strategies\NotContainsSensitiveFilter;
+use Abbasudo\Purity\Filters\Strategies\NotEqualFilter;
+use Abbasudo\Purity\Filters\Strategies\NotInFilter;
+use Abbasudo\Purity\Filters\Strategies\NullFilter;
+use Abbasudo\Purity\Filters\Strategies\StartsWithFilter;
 
 return [
     'filters' => [
         EqualFilter::class,
         InFilter::class,
+        BetweenFilter::class,
+        ContainsFilter::class,
+        EndsWithFilter::class,
+        GreaterThanFilter::class,
+        GreaterOrEqualFilter::class,
+        LessOrEqualFilter::class,
+        LessThanFilter::class,
+        NotContainsFilter::class,
+        NotContainsSensitiveFilter::class,
+        NotEqualFilter::class,
+        NotInFilter::class,
+        NullFilter::class,
+        StartsWithFilter::class,
     ],
 
     'silent' => true,
