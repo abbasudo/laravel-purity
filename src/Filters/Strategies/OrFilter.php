38bdd927160dce0c9fcea009985539742a270e59
phrshteh <frmohammadi007@gmail.com>
2023-02-28 17:24:19 +0330

    add or filter

---
+++ src/Filters/Strategies/OrFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Abbasudo\Purity\Filters\Strategies;
+
+use Abbasudo\Purity\Filters\Filter;
+use Abbasudo\Purity\Filters\Resolve;
+use Closure;
+
+class OrFilter extends Filter
+{
+    /**
+     * Operator string to detect in the query params.
+     *
+     * @var string
+     */
+    protected static string $operator = '$or';
+
+
+    /**
+     * Apply filter logic to $query.
+     *
+     * @return Closure
+     */
+    public function apply(): Closure
+    {
+        return function ($query) {
+            foreach ($this->values as $value) {
+                $query->orWhere(function ($query) use ($value) {
+                    foreach ($value as $key => $item) {
+                        app(Resolve::class)->apply($query, $key, $item);
+                    }
+                });
+            }
+        };
+    }
+}
