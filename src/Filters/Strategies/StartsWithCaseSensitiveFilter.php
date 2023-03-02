0ddcc37c933ed7f39594066ec2a02076edd3e744
phrshteh <frmohammadi007@gmail.com>
2023-03-02 16:42:19 +0330

    add startWith filter (case-sensitive)

---
+++ src/Filters/Strategies/StartsWithCaseSensitiveFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Abbasudo\Purity\Filters\Strategies;
+
+use Abbasudo\Purity\Filters\Filter;
+use Closure;
+
+class StartsWithCaseSensitiveFilter extends Filter
+{
+    /**
+     * Operator string to detect in the query params.
+     *
+     * @var string
+     */
+    protected static string $operator = '$startsWithc';
+
+
+    /**
+     * Apply filter logic to $query.
+     *
+     * @return Closure
+     */
+    public function apply(): Closure
+    {
+        return function ($query) {
+            foreach ($this->values as $value) {
+                $query->whereRaw("BINARY `{$this->column}` like ?", $value . '%');
+            }
+        };
+    }
+}
