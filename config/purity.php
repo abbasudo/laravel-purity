1b50b8c66c9638eea252b64eb06e718dca6f10e5
phrshteh <frmohammadi007@gmail.com>
2023-03-02 17:18:39 +0330

    add filters to config list

--- config/purity.php
+++ config/purity.php
@@ -1,8 +1,12 @@
 <?php
 
+use Abbasudo\Purity\Filters\Strategies\AndFilter;
 use Abbasudo\Purity\Filters\Strategies\BetweenFilter;
+use Abbasudo\Purity\Filters\Strategies\ContainsCaseSensitiveFilter;
 use Abbasudo\Purity\Filters\Strategies\ContainsFilter;
+use Abbasudo\Purity\Filters\Strategies\EndsWithCaseSensitiveFilter;
 use Abbasudo\Purity\Filters\Strategies\EndsWithFilter;
+use Abbasudo\Purity\Filters\Strategies\EqualCaseSensitiveFilter;
 use Abbasudo\Purity\Filters\Strategies\EqualFilter;
 use Abbasudo\Purity\Filters\Strategies\GreaterOrEqualFilter;
 use Abbasudo\Purity\Filters\Strategies\GreaterThanFilter;
@@ -13,7 +17,10 @@ use Abbasudo\Purity\Filters\Strategies\NotContainsFilter;
 use Abbasudo\Purity\Filters\Strategies\NotContainsSensitiveFilter;
 use Abbasudo\Purity\Filters\Strategies\NotEqualFilter;
 use Abbasudo\Purity\Filters\Strategies\NotInFilter;
+use Abbasudo\Purity\Filters\Strategies\NotNullFilter;
 use Abbasudo\Purity\Filters\Strategies\NullFilter;
+use Abbasudo\Purity\Filters\Strategies\OrFilter;
+use Abbasudo\Purity\Filters\Strategies\StartsWithCaseSensitiveFilter;
 use Abbasudo\Purity\Filters\Strategies\StartsWithFilter;
 
 return [
@@ -24,15 +31,22 @@ return [
         ContainsFilter::class,
         EndsWithFilter::class,
         GreaterThanFilter::class,
+        NotNullFilter::class,
+        StartsWithFilter::class,
         GreaterOrEqualFilter::class,
         LessOrEqualFilter::class,
         LessThanFilter::class,
         NotContainsFilter::class,
-        NotContainsSensitiveFilter::class,
         NotEqualFilter::class,
         NotInFilter::class,
         NullFilter::class,
-        StartsWithFilter::class,
+        AndFilter::class,
+        OrFilter::class,
+        NotContainsSensitiveFilter::class,
+        StartsWithCaseSensitiveFilter::class,
+        EndsWithCaseSensitiveFilter::class,
+        EqualCaseSensitiveFilter::class,
+        ContainsCaseSensitiveFilter::class,
     ],
 
     'silent' => true,
