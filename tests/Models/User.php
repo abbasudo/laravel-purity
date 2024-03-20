b7ae8d1b2131c1f680fcbf7931175377fe4fcd29
Lakshan Madushanka <epmadushanka@gmail.com>
2024-03-20 18:21:11 +0530

    Setup required models and data to run tests

---
+++ tests/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Abbasudo\Purity\Tests\Models;
+
+use Abbasudo\Purity\Traits\Filterable;
+use Abbasudo\Purity\Traits\Sortable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class User extends Model
+{
+    use HasFactory;
+    use Filterable;
+    use Sortable;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function post(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function project(): HasOne
+    {
+        return $this->hasOne(Project::class);
+    }
+}
