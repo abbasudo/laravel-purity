019b457218f7f4e0170d3258485930b567aed80a
Abbas mkhzomi <amkhzomi@gmail.com>
2024-06-03 14:39:31 +0330

    add tests

--- tests/Feature/FilterableTest.php
+++ tests/Feature/FilterableTest.php
@@ -2,6 +2,7 @@
 
 use Abbasudo\Purity\Tests\Models\Comment;
 use Abbasudo\Purity\Tests\Models\Post;
+use Abbasudo\Purity\Tests\Models\Product;
 use Abbasudo\Purity\Tests\TestCase;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ class FilterableTest extends TestCase
 
         Route::get('/posts', function () {
             return Post::filter()->get();
+        });
+
+        Route::get('/products', function () {
+            return Product::filter()->get();
         });
 
         Post::create([
@@ -52,7 +57,7 @@ class FilterableTest extends TestCase
     /** @test */
     public function it_can_process_with_grouped_filters()
     {
-        $post = Post::query()->create(['title' => 'title']);
+        $post    = Post::query()->create(['title' => 'title']);
         $comment = Comment::query()->create(['content' => 'comment']);
 
         $post->comments()->save($comment);
@@ -62,5 +67,83 @@ class FilterableTest extends TestCase
             ->assertJsonCount(1);
 
         assertEquals('title', $response->json()[0]['title']);
+    }
+
+    /** @test */
+    public function it_can_filter_by_decimal_values_with_results()
+    {
+        $product = Product::factory()->create([
+            'price' => 1.66,
+        ]);
+
+        $response = $this->getJson('/products?filters[price][$eq]=1.66')
+            ->assertOk()
+            ->assertJsonCount(1);
+
+        assertEquals($product->id, $response->json()[0]['id']);
+    }
+
+    /** @test */
+    public function it_can_filter_by_decimal_values_without_matches()
+    {
+        $product = Product::factory()->create([
+            'price' => 1.66,
+        ]);
+
+        $response = $this->getJson('/products?filters[price][$eq]=1.47')
+            ->assertOk()
+            ->assertJsonCount(0);
+    }
+
+    /** @test */
+    public function it_can_filter_by_float_values_with_results()
+    {
+        $product = Product::factory()->create([
+            'rate' => 859.77,
+        ]);
+
+        $response = $this->getJson('/products?filters[rate][$eq]=859.77')
+            ->assertOk()
+            ->assertJsonCount(1);
+
+        assertEquals($product->id, $response->json()[0]['id']);
+    }
+
+    /** @test */
+    public function it_can_filter_by_float_values_without_matches()
+    {
+        $product = Product::factory()->create([
+            'rate' => 49.79,
+        ]);
+
+        $response = $this->getJson('/products?filters[rate][$eq]=458.57')
+            ->assertOk()
+            ->assertJsonCount(0);
+    }
+
+    /** @test */
+    public function it_can_filter_by_boolean_values_with_results()
+    {
+        $product = Product::factory()->create([
+            'is_available' => true,
+        ]);
+
+        $response = $this->getJson('/products?filters[is_available][$eq]=1')
+            ->assertOk()
+            ->assertJsonCount(1);
+
+        assertEquals($product->id, $response->json()[0]['id']);
+    }
+
+    /** @test */
+    public function it_can_filter_by_boolean_values_without_matches()
+    {
+        $product = Product::factory()->create([
+            'is_available' => true,
+        ]);
+
+        $response = $this->getJson('/products?filters[is_available][$eq]=0')
+            ->assertOk()
+            ->assertJsonCount(0);
     }
 }
