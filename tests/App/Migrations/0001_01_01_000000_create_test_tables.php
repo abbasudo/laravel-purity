1499b92290662718c2b939315370810359d62e35
Abbas mkhzomi <amkhzomi@gmail.com>
2024-06-03 19:54:01 +0330

    move migrations to another folder

---
+++ tests/App/Migrations/0001_01_01_000000_create_test_tables.php
@@ -0,0 +1,98 @@
+<?php
+
+use Abbasudo\Purity\Tests\App\Models\Author;
+use Abbasudo\Purity\Tests\App\Models\Post;
+use Abbasudo\Purity\Tests\App\Models\Tag;
+use Abbasudo\Purity\Tests\App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->string('title')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Post::class)->nullable();
+            $table->foreignIdFor(Tag::class)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Post::class)->nullable();
+            $table->string('content');
+            $table->timestamps();
+        });
+
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Author::class)->nullable();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price');           //testing decimal filtering
+            $table->float('rate');              //testing float filtering
+            $table->boolean('is_available');    //testing boolean filtering
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('comments');
+        Schema::dropIfExists('books');
+        Schema::dropIfExists('authors');
+        Schema::dropIfExists('products');
+    }
+};
