6939315278821933c39e8295e08c3cc1a5bd3691
Abbas mkhzomi <amkhzomi@gmail.com>
2024-06-03 18:47:27 +0330

    fix strategies

--- src/Filters/Strategies/NotContainsSensitiveFilter.php
+++ src/Filters/Strategies/NotContainsSensitiveFilter.php
@@ -4,6 +4,8 @@ namespace Abbasudo\Purity\Filters\Strategies;
 
 use Abbasudo\Purity\Filters\Filter;
 use Closure;
+use Illuminate\Support\Facades\DB;
+use RuntimeException;
 
 class NotContainsSensitiveFilter extends Filter
 {
@@ -22,8 +24,25 @@ class NotContainsSensitiveFilter extends Filter
     public function apply(): Closure
     {
         return function ($query) {
+            $connection = DB::connection()->getDriverName();
+
             foreach ($this->values as $value) {
-                $query->where("BINARY `{$this->column}` not like ?", ["%{$value}%"]);
+                switch ($connection) {
+                    case 'mysql':
+                        $query->whereRaw("BINARY `{$this->column}` not like ?", ["%{$value}%"]);
+                        break;
+                    case 'sqlite':
+                        $query->whereRaw("`{$this->column}` COLLATE BINARY not like ?", ["%{$value}%"]);
+                        break;
+                    case 'pgsql':
+                        $query->whereRaw("`{$this->column}`::bytea not LIKE ?", ["%{$value}%"]);
+                        break;
+                    case 'sqlsrv':
+                        $query->whereRaw("`{$this->column}` COLLATE Latin1_General_BIN not LIKE ?", ["%{$value}%"]);
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported database driver: {$connection}");
+                }
             }
         };
     }
